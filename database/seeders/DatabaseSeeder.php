2e5c01d7ae57dafcd4385bb36f79a1b419703feb
AumPanudach <panudach.work@gmail.com>
2025-11-25 23:36:24 +0700

    Disable database seeder to prevent automatic data seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $this->call(CategoryTableSeeder::class);
-         $this->call(ProductTableSeeder::class);
+        // Seeder disabled - data should not be seeded automatically
+        // $this->call(CategoryTableSeeder::class);
+        // $this->call(ProductTableSeeder::class);
     }
 }
